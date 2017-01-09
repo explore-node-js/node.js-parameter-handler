e8474e32ed244e95faccf07419a761c46e5dd0a4
Eugene Matvejev <eugene.matvejev@gmail.com>
2017-01-09 01:38:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Processor = require('./src/processor');
 
 let packageJsonPath = `${process.cwd()}/package.json`,
     packageJsonContent = fs.readFileSync(packageJsonPath),
-/** @param {{extra: {node_parameter_handler: []}}} content */
+    /** @param {{extra: {node_parameter_handler: []}}} content */
     packageJson = JSON.parse(packageJsonContent);
 
 try {
@@ -15,12 +15,33 @@ try {
     if(undefined === packageJson.extra.node_parameter_handler) {
         throw `node 'node_parameter_handler' in 'extra' is not defined`
     }
-    if(undefined === packageJson.extra.node_parameter_handler) {
-        throw `node 'node_parameter_handler' in 'extra' is not defined`
+    if(!Array.isArray(packageJson.extra.node_parameter_handler)) {
+        throw `node 'node_parameter_handler' in 'extra' is not array`
     }
+
+    const processor = new Processor(packageJson.extra.node_parameter_handler, process.cwd());
+
+    processor.process();
+    processor.write();
+} catch (e) {
+    console.log(`
+        ${e}
+        
+        example of package.json:
+        {
+            ...
+            "extra": {
+                "node_parameter_handler": [
+                    ...
+                    {
+                        "source": "src/parameters.json.dist",
+                        "output": "src/parameters.json",
+                        "envMap": {
+                            "node_path": "ENV_VARIABLE"
+                        }
+                    }
+                ]
+            }            
+        }
+    `)
 }
-
-const processor = new Processor(packageJson.extra.node_parameter_handler, process.cwd());
-
-processor.process();
-processor.write();
