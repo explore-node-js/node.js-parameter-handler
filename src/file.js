7368b0b3115ab7f2c67e59ee419299fb4b41c993
Eugene Matvejevs <eugene.matvejev@gmail.com>
2017-02-19 18:35:12 +0000

    implement better wrapper for files

--- src/file.js
+++ src/file.js
@@ -1,2 +1,61 @@
 module.exports = class File {
-}
+    constructor() {
+        this.sourcePath = '';
+        this.outputPath = '';
+        this.content = '';
+    }
+
+    /**
+     * @returns {string}
+     */
+    getSourcePath() {
+        return this.sourcePath;
+    }
+
+    /**
+     * @param {string} path
+     *
+     * @returns {File}
+     */
+    setSourcePath(path) {
+        this.sourcePath = path;
+
+        return this;
+    }
+
+    /**
+     * @returns {string}
+     */
+    getOutputPath() {
+        return this.outputPath;
+    }
+
+    /**
+     * @param {string} path
+     *
+     * @returns {File}
+     */
+    setOutputPath(path) {
+        this.outputPath = path;
+
+        return this;
+    }
+
+    /**
+     * @returns {string}
+     */
+    getContent() {
+        return this.content;
+    }
+
+    /**
+     * @param {string} content
+     *
+     * @returns {File}
+     */
+    setContent(content) {
+        this.content = content;
+
+        return this;
+    }
+};
