5d3edecca1ac4a8b5035a9da1f9170c092a9432b
Eugene Matvejev <eugene.matvejev@gmail.com>
2018-05-16 09:04:15 +0100

    1.0.1 [proper]

--- tests/processor.test.js
+++ tests/processor.test.js
@@ -8,10 +8,30 @@ describe('processor', () => {
     describe('::resolvePath', () => {
         const processor = new Processor(config, cwd);
 
-        [ '/', 'test', 'test/', 'test/test', '/test/test' ].forEach(path => {
+        [ '/', 'test', 'test/', 'test/test', '/test/test' ].forEach((path) => {
             it(`should be converted '${path}' to absolute path`, () => {
                 expect(processor.resolvePath(path)).toMatch(/^\/.*/);
-            })
+            });
         });
-    })
+    });
+
+    describe('::getEnvironmentValue', () => {
+        [
+            { env: 'NODE_ENV', expected: 'test' },
+            { env: 'NODE_ENV_NOT_EXISTS', expected: undefined },
+        ].forEach(({ env, expected }) => {
+            it(`should be return '${expected}' when read value of '${env}' env. variable`, () => {
+                expect(Processor.getEnvironmentValue(env)).toBe(expected);
+            });
+        });
+    });
+
+    describe('::process', () => {
+        const processor = new Processor(config, cwd);
+
+        it(`match snapshot`, () => {
+            processor.process();
+            expect(processor.files).toMatchSnapshot();
+        });
+    });
 });
